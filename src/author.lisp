e5a52a2710c959115ca8bd3d63ee616ff64a4b39
SeungKi Kim <tttuuu888@gmail.com>
2019-11-17 22:25:45 +0900

    Add post, author files

---
+++ src/author.lisp
@@ -0,0 +1,6 @@
+(in-package :web-test3)
+
+
+(defun get-author-of-post (post-id)
+  "Return an information of the author of the post."
+  ())
---
+++ src/post.lisp
@@ -0,0 +1,9 @@
+(in-package :web-test3)
+
+
+(defun get-post-list (&optional page)
+  "Return a list of post titles."
+  '("title test 1"
+    "title test 2"
+    "Another title"
+    "Yet another title"))
