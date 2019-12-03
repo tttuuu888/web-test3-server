11e33de58e3a0dcc8364d379f5adc49ef8dd11f8
SeungKi Kim <tttuuu888@gmail.com>
2019-12-04 04:41:34 +0900

    Update login

--- src/user.lisp
+++ src/user.lisp
@@ -23,16 +23,24 @@
                       (make-json :id id
                                  :nickname nickname)))))))
 
+(defun user-valid-p (id password)
+  (let ((user (db-user-find id)))
+    (and user (equal password (slot-value user 'password)))))
+
 (defun user-login (json)
-  (let ((id (st-json:getjso "id" json))
-        (password (st-json:getjso "password" json)))
+  (let* ((id (st-json:getjso "id" json))
+        (password (st-json:getjso "password" json))
+        (user (db-user-find id)))
     ;; (format t "id : ~a, pw : ~a session id :~a ~%" id password (hunchentoot:session-value :id))
-    (hunchentoot:start-session)
-    (setf (hunchentoot:session-value :id) id)
-    (st-json:write-json-to-string
-     (make-json :user
-                (make-json :id id
-                           :nickname "test-nick")))))
+    (if (not (and user (equal password (slot-value user 'password))))
+        (make-error-json "user-not-valid")
+        (progn
+          (hunchentoot:start-session)
+          (setf (hunchentoot:session-value :id) id)
+          (st-json:write-json-to-string
+           (make-json :user
+                      (make-json :id id
+                                 :nickname (slot-value user 'nickname))))))))
 
 (defun user-logout (json)
   (let ((id (st-json:getjso "id" json)))
